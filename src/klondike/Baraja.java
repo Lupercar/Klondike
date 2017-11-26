bbdfc642526f109720c0eceb26388edd5f0a4052
Lupercar <hotolete@gmail.com>
2017-11-26 18:03:17 +0100

    Mejoro clase Klondike y creo clase Intervalo

--- src/klondike/Baraja.java
+++ src/klondike/Baraja.java
@@ -2,6 +2,20 @@ package klondike;
 
 public class Baraja {
 
+	public static final int NUM_PALOS = 4;
+	private Carta[] cartas; 
+	
+	public Baraja(){
+		cartas = new Carta[52]; 
+		int contador = 0;
+		for(int i = 0; i<4;i++){
+			for(int j = 0; j<13; j++){
+				cartas[] = new Carta(i,j);
+				contador++; 
+			}
+		}
+	}
+	
 	public void mostrar() {
 		// TODO Auto-generated method stub
 		
--- src/klondike/Klondike.java
+++ src/klondike/Klondike.java
@@ -6,17 +6,19 @@ public class Klondike {
 	private Palo[] palos; // tengo 4 palos
 	private Columna[] columnas;
 	private Descarte descarte;
+	
+	private static final int NUM_COLUMNAS = 7; 
 
 	private Klondike() {
 		baraja = new Baraja();
 		descarte = new Descarte();
 
-		palos = new Palo[4];
+		palos = new Palo[Baraja.NUM_PALOS];
 		for (int i = 0; i < palos.length; i++) {
 			palos[i] = new Palo();
 		}
 
-		columnas = new Columna[7];
+		columnas = new Columna[NUM_COLUMNAS];
 		for (int i = 0; i < columnas.length; i++) {
 			columnas[i] = new Columna(i + 1, baraja);
 		}
@@ -25,13 +27,13 @@ public class Klondike {
 	private void jugar() {
 		// Le pongo un menu con las opciones
 		// Me creo una Clase Menu
-		GestorIo gestorIo = new GestorIo();
 		Menu menu = new Menu();
 		int opcion;
 
 		do {
 			// método privado mostrar estado()
 			this.mostrar();
+			menu.mostrar();
 			
 			//le paso una opcion
 			opcion = menu.getOpcion(); 
@@ -43,43 +45,71 @@ public class Klondike {
 				baraja.moverA(descarte); 
 				break;
 			case 2:
-				descarte.moverA(palos);
+				descarte.moverA(this.recogerPalo("A"));
 				break;
 			case 3:
-				descarte.moverA(columnas);
+				descarte.moverA(this.recogerColumna("A"));
 				break;
 			case 4:
-				gestorIo.out("De que palo? [1-4]: ");
-				Palo palo = palos[gestorIo.inInt()-1];
-				gestorIo.out("De que columna? [1-7]: ");
-				Columna columna = columnas[gestorIo.inInt()-1];
-				palo.moverA(columna);
+				this.recogerPalo("De").moverA(this.recogerColumna("A"));
 				break;
 			case 5:
+				this.recogerColumna("De").moverA(this.recogerPalo("A"));
 				break;
 			case 6:
+				this.recogerColumna("De").moverA(this.recogerColumna("A"));
 				break;
 			case 7:
+				this.recogerColumna("De").voltear();
 				break;
 			case 8:
+				descarte.voltear(baraja);
 				break;
 			case 9:
 				break;
 
-			default:
-				break;
 			}
 		} while (opcion != 9);
+	}
+
+	private Columna recogerColumna(String prefijo) {
+		GestorIo gestorIo = new GestorIo();
+		int columna; 
+		boolean error; 
+		do{
+			gestorIo.out("¿" + prefijo + " qué columna? [1-" + NUM_COLUMNAS + "]:" ); 
+			columna = gestorIo.inInt(); 
+			error = !new Intervalo(1,NUM_COLUMNAS).incluye(columna); 
+			if(error){
+				gestorIo.out("Error!!! Debe ser un número entero entre 1 y " + NUM_COLUMNAS);
+			}
+		}while(error);
+		return columnas[columna-1];
+	}
+
+	private Palo recogerPalo(String prefijo) {
+		GestorIo gestorIo = new GestorIo();
+		int palo; 
+		boolean error; 
+		do{
+			gestorIo.out("¿" + prefijo + " qué palo? [1-" + Baraja.NUM_PALOS + "]:" ); 
+			palo = gestorIo.inInt(); 
+			error = !new Intervalo(1,Baraja.NUM_PALOS).incluye(palo); 
+			if(error){
+				gestorIo.out("Error!!! Debe ser un número entero entre 1 y " + Baraja.NUM_PALOS);
+			}
+		}while(error);
+		return palos[palo-1];
 	}
 
 	private void mostrar() {
 		baraja.mostrar();
 		descarte.mostrar();
-		for (int i = 0; i < palos.length; i++) {
-			palos[i].mostrar();
+		for (Palo palo : palos) {
+			palo.mostrar();
 		}
-		for (int i = 0; i < columnas.length; i++) {
-			columnas[i].mostrar();
+		for (Columna columna : columnas) {
+			columna.mostrar();
 		}
 	}
 
@@ -87,4 +117,4 @@ public class Klondike {
 		new Klondike().jugar();
 	}
 
-}//1:44:17
+}
